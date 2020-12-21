e3f4349b02cad65e7d334f3761b221f5d15e38da
rahul singh <rsinghsep@gmail.com>
2020-12-21 22:57:34 +0530

    updated TaskDao interface

--- app/src/main/java/com/staysilly/geotouristapp/repositories/TourDao.java
+++ app/src/main/java/com/staysilly/geotouristapp/repositories/TourDao.java
@@ -1,8 +1,23 @@
 package com.staysilly.geotouristapp.repositories;
 
+import com.staysilly.geotouristapp.models.Tour;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
 
 @Dao
-public abstract class TourDao {
+public interface TourDao {
+
+    /*/////////////////////////////////////////////////
+    //PROPERTY
+    /*/////////////////////////////////////////////////
+    @Insert
+    void insertTour(Tour tour);
+    @Query("SELECT * FROM tour_table")
+    LiveData<List<Tour>> getAllTours();
 
 }
