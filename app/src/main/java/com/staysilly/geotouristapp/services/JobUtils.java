ca6af0c5ce91aac97c76569c7c9f4a66126445a1
rahul singh <rsinghsep@gmail.com>
2020-12-22 18:59:21 +0530

    added method to start the job scheduler

--- app/src/main/java/com/staysilly/geotouristapp/services/JobUtils.java
+++ app/src/main/java/com/staysilly/geotouristapp/services/JobUtils.java
@@ -1,18 +1,34 @@
 package com.staysilly.geotouristapp.services;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 class JobUtils {
 
     /*/////////////////////////////////////////////////
     //MEMBERS
     /*/////////////////////////////////////////////////
     private final String TAG = "**JobUtils";
+    private static final int ONE_SECOND_IN_MS = 1000;
+    private static final int ONE_MINUTE_IN_MS = ONE_SECOND_IN_MS*60;
 
 
     /*/////////////////////////////////////////////////
     //PUBLIC METHODS
     /*/////////////////////////////////////////////////
-    public static void scheduleLocationTrackingJob(){
-        // TODO: 12/22/20
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static void scheduleLocationTrackingJob(Context context){
+        ComponentName componentName = new ComponentName(context, TrackerJobService.class);
+        JobInfo.Builder builder = new JobInfo.Builder(0, componentName);
+        builder.setMinimumLatency(13*ONE_MINUTE_IN_MS);
+        builder.setOverrideDeadline(17*ONE_MINUTE_IN_MS);
+        JobScheduler scheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        scheduler.schedule(builder.build());
     }
 
 }
--- app/src/main/java/com/staysilly/geotouristapp/services/TrackerJobService.java
+++ app/src/main/java/com/staysilly/geotouristapp/services/TrackerJobService.java
@@ -3,6 +3,7 @@ package com.staysilly.geotouristapp.services;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 
@@ -12,7 +13,7 @@ class TrackerJobService extends JobService {
     /*/////////////////////////////////////////////////
     //MEMBERS
     /*/////////////////////////////////////////////////
-    private final String TAG = this.getClass().getSimpleName();
+    private final String TAG = "**"+this.getClass().getSimpleName();
 
 
     /*/////////////////////////////////////////////////
@@ -20,11 +21,13 @@ class TrackerJobService extends JobService {
     /*/////////////////////////////////////////////////
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
-        return false;
+        Log.d(TAG, "onStartJob");
+        return true;
     }
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
-        return false;
+        Log.d(TAG, "onStopJob");
+        return true;
     }
 
 }
